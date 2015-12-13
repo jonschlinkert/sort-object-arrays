6162b2a61ec1c0d512e9f972025ce8e594e414dc
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-12 23:48:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * sort-object-arrays <https://github.com/jonschlinkert/sort-object-arrays>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
